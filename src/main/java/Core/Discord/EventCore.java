e37c9b1a913e6855d30f67c7f4febe1934e31d0d
etsubu <etsubu@gmail.com>
2020-09-11 15:53:21 +0300

    Add channel restriction configurations

--- src/main/java/Core/Discord/EventCore.java
+++ src/main/java/Core/Discord/EventCore.java
@@ -3,6 +3,7 @@ package Core.Discord;
 import Core.Commands.CommandHandler;
 import Core.Commands.CommandResult;
 import Core.Configuration.ConfigLoader;
+import Core.Permissions.PermissionManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -25,14 +26,16 @@ public class EventCore extends ListenerAdapter
     private static final Logger log = LoggerFactory.getLogger(EventCore.class);
     private final CommandHandler commandHandler;
     private final JDA jda;
+    private final PermissionManager permissionManager;
 
     /**
      * Initializes EventCore
      */
-    public EventCore(CommandHandler commandHandler, ConfigLoader configLoader) throws LoginException {
+    public EventCore(CommandHandler commandHandler, ConfigLoader configLoader, PermissionManager permissionManager) throws LoginException {
         this.jda = JDABuilder.createDefault(configLoader.getConfig().getOauth()).build();
         jda.addEventListener(this);
         this.commandHandler = commandHandler;
+        this.permissionManager = permissionManager;
         log.info("Discord name: {}", jda.getSelfUser().getName());
     }
     
@@ -41,6 +44,11 @@ public class EventCore extends ListenerAdapter
     {
         if(event.getAuthor().isBot() || event.getAuthor().getName().equalsIgnoreCase(jda.getSelfUser().getName())) {
             // Skip messages sent by bots or ourselves
+            return;
+        }
+        if(!permissionManager.isReplyAllowed(event)) {
+            log.info("Prevented reply for {}  on channel {}", event.getAuthor().getName(), event.getChannel().getName());
+            event.getChannel().sendMessage("Please use whitelisted channel for performing commands").queue();
             return;
         }
         if (this.commandHandler.isCommand(event.getMessage().getContentDisplay())) {
