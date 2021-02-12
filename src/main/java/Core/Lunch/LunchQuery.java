95526443c48928af941511e1c4df60b0e20ed6f5
etsubu <etsubu@gmail.com>
2021-02-12 20:49:00 +0200

    Added altman z-score as a command

--- src/main/java/Core/Lunch/LunchQuery.java
+++ src/main/java/Core/Lunch/LunchQuery.java
@@ -7,12 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.DayOfWeek;
-import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -24,7 +19,6 @@ public class LunchQuery {
     private final ZoneId zoneId = ZoneId.of("Europe/Helsinki");
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private static final Logger log = LoggerFactory.getLogger(LunchQuery.class);
-    private final HttpClient client;
     private final Gson gson;
     private final List<Pair<String, String>> restaurants;
 
@@ -32,8 +26,6 @@ public class LunchQuery {
         this.restaurants = new ArrayList<>();
         restaurants.add(new Pair<>("https://www.semma.fi/api/restaurant/menu/day?date=%s&language=fi&restaurantPageId=207735", "Ravintola Piato"));
         restaurants.add(new Pair<>("https://www.semma.fi/api/restaurant/menu/day?date=%s&language=fi&restaurantPageId=207659", "Ravintola Maija"));
-        this.client = HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.NORMAL).build();
         this.gson = new Gson();
     }
 
