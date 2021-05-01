165c282bc96a5195922f3f0d71ccffe2bc3e123f
etsubu <etsubu@gmail.com>
2021-05-01 22:00:06 +0300

    Clone input byte buffer argument in AttachmentFile constructor

--- src/main/java/com/etsubu/stonksbot/scheduler/omxnordic/Model/AttachmentFile.java
+++ src/main/java/com/etsubu/stonksbot/scheduler/omxnordic/Model/AttachmentFile.java
@@ -1,12 +1,15 @@
 package com.etsubu.stonksbot.scheduler.omxnordic.Model;
 
 import com.etsubu.stonksbot.utility.TypeUtils;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public class AttachmentFile {
     private final byte[] file;
     private final String filename;
+
+    public AttachmentFile(byte[] file, String filename) {
+        this.file = TypeUtils.cloneByteArray(file);
+        this.filename = filename;
+    }
 
     public String getFilename() { return filename; }
 
