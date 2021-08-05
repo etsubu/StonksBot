3f71999aa4a2c09037fc996c4e3474edb434022b
etsubu <etsubu@gmail.com>
2021-08-05 19:59:12 +0300

    Improve system test stability

--- src/integration_test/java/com/etsubu/stonksbot/SystemTest.java
+++ src/integration_test/java/com/etsubu/stonksbot/SystemTest.java
@@ -2,14 +2,15 @@ package com.etsubu.stonksbot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.ISnowflake;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
@@ -17,9 +18,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD;
 
 /**
  * System test implementation that launches bot instance with valid configurations and invokes commands on a test server
@@ -29,11 +32,12 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
+@Execution(SAME_THREAD)
 public class SystemTest extends ListenerAdapter {
     private static final Logger log = LoggerFactory.getLogger(SystemTest.class);
     private static JDA jda;
-    private Message lastMessage;
     private static TextChannel channel;
+    private static final Map<String, Message> messages = Collections.synchronizedMap(new HashMap<>());
 
     /**
      * Initializes both discord bot client instances and pulls oath tokens from env variables, oath1= token used by the tester instance that issues commands
@@ -54,185 +58,195 @@ public class SystemTest extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if(event.getAuthor().getId().equals(jda.getSelfUser().getId())) {
+        if(event.getAuthor().getId().equals(jda.getSelfUser().getId()) || !event.getAuthor().isBot()) {
             return;
         }
-        lastMessage = event.getMessage();
-        event.getGuild().getMembers().forEach(x -> log.info("{}:{}", x.getEffectiveName(), x.getOnlineStatus()));
+        String id = Optional.ofNullable(event.getMessage().getReferencedMessage()).map(ISnowflake::getId).orElse(event.getMessageId());
+        messages.put(id, event.getMessage());
         log.info("Received message: {}", event.getMessage().getContentDisplay());
     }
 
-    private Message readMessage() {
+    private Message readMessage(String id) {
         long until = System.currentTimeMillis() + 5000;
-        Message msg = null;
-        while(System.currentTimeMillis() < until && (msg = lastMessage) == null) {
+        while(System.currentTimeMillis() < until && !messages.containsKey(id)) {
             try {
                 Thread.sleep(1);
             } catch (InterruptedException ignored) {
             }
         }
-        lastMessage = null;
-        return msg;
+        return messages.remove(id);
     }
 
     @Test
-    public void testPriceCommand() {
+    public void testPriceCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!p msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("MSFT") && msg.getContentDisplay().contains("Price") &&
-                msg.getContentDisplay().contains("Change") &&
-                msg.getContentDisplay().contains("Open") &&
-                msg.getContentDisplay().contains("High") &&
-                msg.getContentDisplay().contains("Low") &&
-                msg.getContentDisplay().contains("Volume"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!p msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("MSFT") && response.getContentDisplay().contains("Price") &&
+                response.getContentDisplay().contains("Change") &&
+                response.getContentDisplay().contains("Open") &&
+                response.getContentDisplay().contains("High") &&
+                response.getContentDisplay().contains("Low") &&
+                response.getContentDisplay().contains("Volume"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testStatsCommand() {
+    public void testStatsCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!s msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("MSFT") && msg.getContentDisplay().contains("\nForward P/E: ") &&
-                msg.getContentDisplay().contains("\nPEG: ") &&
-                msg.getContentDisplay().contains("\nBeta: ") &&
-                msg.getContentDisplay().contains("\nTrailing EPS: ") &&
-                msg.getContentDisplay().contains("\nForward EPS: ") &&
-                msg.getContentDisplay().contains("\nForecasted EPS Growth: ") &&
-                msg.getContentDisplay().contains("\nProfit margin: ") &&
-                msg.getContentDisplay().contains("\nShort percent of float: ") &&
-                msg.getContentDisplay().contains("\nRecent change in short ratio: ") &&
-                msg.getContentDisplay().contains("\nHeld by insiders: ") &&
-                msg.getContentDisplay().contains("\nHeld by institutions: ") &&
-                msg.getContentDisplay().contains("\n52 week price change: "));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!s msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("MSFT") && response.getContentDisplay().contains("\nForward P/E: ") &&
+                response.getContentDisplay().contains("\nPEG: ") &&
+                response.getContentDisplay().contains("\nBeta: ") &&
+                response.getContentDisplay().contains("\nTrailing EPS: ") &&
+                response.getContentDisplay().contains("\nForward EPS: ") &&
+                response.getContentDisplay().contains("\nForecasted EPS Growth: ") &&
+                response.getContentDisplay().contains("\nProfit margin: ") &&
+                response.getContentDisplay().contains("\nShort percent of float: ") &&
+                response.getContentDisplay().contains("\nRecent change in short ratio: ") &&
+                response.getContentDisplay().contains("\nHeld by insiders: ") &&
+                response.getContentDisplay().contains("\nHeld by institutions: ") &&
+                response.getContentDisplay().contains("\n52 week price change: "));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testRatiosCommand() {
+    public void testRatiosCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!r msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("MSFT") && msg.getContentDisplay().contains("\nEV/Revenue:") &&
-                msg.getContentDisplay().contains("\nEV/EBITDA") &&
-                msg.getContentDisplay().contains("\nEV/EBIT") &&
-                msg.getContentDisplay().contains("\nMCAP/FCF") &&
-                msg.getContentDisplay().contains("\nGross Profit Margin: ") &&
-                msg.getContentDisplay().contains("\nCurrent ratio: ") &&
-                msg.getContentDisplay().contains("\nROA: ") &&
-                msg.getContentDisplay().contains("\nP/S: ") &&
-                msg.getContentDisplay().contains("\nP/E: ") &&
-                msg.getContentDisplay().contains("\nP/B: "));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!r msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("MSFT") && response.getContentDisplay().contains("\nEV/Revenue:") &&
+                response.getContentDisplay().contains("\nEV/EBITDA") &&
+                response.getContentDisplay().contains("\nEV/EBIT") &&
+                response.getContentDisplay().contains("\nMCAP/FCF") &&
+                response.getContentDisplay().contains("\nGross Profit Margin: ") &&
+                response.getContentDisplay().contains("\nCurrent ratio: ") &&
+                response.getContentDisplay().contains("\nROA: ") &&
+                response.getContentDisplay().contains("\nP/S: ") &&
+                response.getContentDisplay().contains("\nP/E: ") &&
+                response.getContentDisplay().contains("\nP/B: "));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testCalendarCommand() {
+    public void testCalendarCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!c msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("MSFT") && msg.getContentDisplay().contains("\nEarnings date:") &&
-                msg.getContentDisplay().contains("\nForecast EPS avg") &&
-                msg.getContentDisplay().contains("\nForecast EPS high") &&
-                msg.getContentDisplay().contains("\nForecast EPS low") &&
-                msg.getContentDisplay().contains("\nForecast Revenue avg: ") &&
-                msg.getContentDisplay().contains("\nForecast Revenue high: ") &&
-                msg.getContentDisplay().contains("\nForecast Revenue low: ") &&
-                msg.getContentDisplay().contains("\nPrevious dividend date: ") &&
-                msg.getContentDisplay().contains("\nNext dividend date: "));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!c msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("MSFT") && response.getContentDisplay().contains("\nEarnings date:") &&
+                response.getContentDisplay().contains("\nForecast EPS avg") &&
+                response.getContentDisplay().contains("\nForecast EPS high") &&
+                response.getContentDisplay().contains("\nForecast EPS low") &&
+                response.getContentDisplay().contains("\nForecast Revenue avg: ") &&
+                response.getContentDisplay().contains("\nForecast Revenue high: ") &&
+                response.getContentDisplay().contains("\nForecast Revenue low: ") &&
+                response.getContentDisplay().contains("\nPrevious dividend date: ") &&
+                response.getContentDisplay().contains("\nNext dividend date: "));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testBioCommand() {
+    public void testBioCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!b msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!b msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testListCommand() {
+    public void testListCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!commands").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().split("\n").length > 5);
-        assertTrue(Arrays.stream(msg.getContentDisplay().split("\n")).allMatch(x -> x.startsWith("!")));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!commands");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().split("\n").length > 5);
+        assertTrue(Arrays.stream(response.getContentDisplay().split("\n")).allMatch(x -> x.startsWith("!")));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testFscore() {
+    public void testFscore() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!f msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("\nScore:"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!f msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("\nScore:"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testZscore() {
+    public void testZscore() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!z msft").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("\nZ-Score:"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!z msft");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("\nZ-Score:"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testAboutCommand() {
+    public void testAboutCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!about").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().contains("Author"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!about");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().contains("Author"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testRecommendationCommand() {
+    public void testRecommendationCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!suositus outokumpu").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().startsWith("```") && msg.getContentDisplay().endsWith("```"));
-        assertTrue(msg.getContentDisplay().contains("(Inderes)"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!suositus outokumpu");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().startsWith("```") && response.getContentDisplay().endsWith("```"));
+        assertTrue(response.getContentDisplay().contains("(Inderes)"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testHelpCommand() {
+    public void testHelpCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!help help").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().contains("Example"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!help help");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().contains("Example"));
+        log.info(response.getContentDisplay());
     }
 
     @Test
-    public void testVotelinkCommand() {
+    public void testVotelinkCommand() throws ExecutionException, InterruptedException {
         jda.addEventListener(this);
-        channel.sendMessage("!lunch").queue();
-        Message msg = readMessage();
-        assertNotNull(msg);
-        assertTrue(msg.getContentDisplay().contains("Ravintola"));
-        log.info(msg.getContentDisplay());
+        var action = channel.sendMessage("!lunch");
+        Message message = action.submit().get();
+        Message response = readMessage(message.getId());
+        assertNotNull(response);
+        assertTrue(response.getContentDisplay().contains("Ravintola"));
+        log.info(response.getContentDisplay());
     }
 }
